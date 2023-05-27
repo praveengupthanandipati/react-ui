7099795c8b3dba8931d472a19259ca7f46951d6f
Praveen Guptha <56840552+praveengupthanandipati@users.noreply.github.com>
2023-05-27 10:38:45 +0530

    commit files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./assets/styles/App.scss";
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <h1> My React Samples</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
